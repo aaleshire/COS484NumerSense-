d2c3b68cd7a18ac114983b4a1f20213f80314fdd
aaleshire <98134244+aaleshire@users.noreply.github.com>
2023-05-04 17:32:27 -0400

    pls

--- happy-transformer/happytransformer/__init__.py
+++ happy-transformer/happytransformer/__init__.py
@@ -1,7 +1,6 @@
 from happytransformer.happy_roberta import HappyROBERTA
 from happytransformer.happy_xlnet import HappyXLNET
 from happytransformer.happy_bert import HappyBERT
-from happytransformer.happy_albert import HappyALBERT
 from happytransformer.classifier_args import classifier_args
 
 name = "happytransformer"
