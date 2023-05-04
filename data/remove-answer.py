a5026b9d6bae5a51a0501299cb08084336c30409
aaleshire <98134244+aaleshire@users.noreply.github.com>
2023-05-04 16:26:00 -0400

    fixed test data

--- data/remove-answer.py
+++ data/remove-answer.py
@@ -1,5 +1,5 @@
 input_file = 'old-data/test_gkb_best_filtered.tsv'
-output_file = 'old-data/test_gkb_best_filterd_answer_removed.txt'
+output_file = 'old-data/test_gkb_best_filtered_answer_removed.txt'
 
 with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
     for line in infile:
