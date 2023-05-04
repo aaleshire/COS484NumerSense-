73a10942839e5273fb2639609ab6c9c6c56ba1bb
aaleshire <98134244+aaleshire@users.noreply.github.com>
2023-05-04 15:43:48 -0400

    normal w 80%

--- src/finetune_mlm.py
+++ src/finetune_mlm.py
@@ -218,8 +218,7 @@ def mask_tokens(inputs: torch.Tensor, tokenizer: PreTrainedTokenizer, args) -> T
 
 def mask_num_tokens(inputs: torch.Tensor, tokenizer: PreTrainedTokenizer, args) -> Tuple[torch.Tensor, torch.Tensor]:
     """ Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original. """
-    # num_list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "no", "zero"]
-    num_list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "zero"]
+    num_list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "no", "zero"]
 
     if tokenizer.mask_token is None:
         raise ValueError(
@@ -243,7 +242,19 @@ def mask_num_tokens(inputs: torch.Tensor, tokenizer: PreTrainedTokenizer, args) 
 
     masked_indices = torch.BoolTensor(masked_indices)
     labels[~masked_indices] = -100  # We only compute loss on masked tokens
-    inputs[masked_indices] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+    # inputs[masked_indices] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+
+    # adding this?
+    # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+
+    # 10% of the time, we replace masked input tokens with random word
+    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    inputs[indices_random] = random_words[indices_random]
+
+    # The rest of the time (10% of the time) we keep the masked input tokens unchanged
 
     return inputs, labels
 
